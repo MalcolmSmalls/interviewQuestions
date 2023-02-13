42ca5ee8392214ea0f2e32ea583942d01399fb66
MalcolmSmalls <smallsbeats@gmail.com>
2023-02-13 14:12:23 -0800

    Add selectionSort solution

--- js/main.js
+++ js/main.js
@@ -25,3 +25,22 @@ function sameFrequency(num1, num2) {
 }
 
 console.log(sameFrequency(182, 281))
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minimum = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minimum]) {
+        minimum = j
+      }
+    }
+    if (minimum !== i) {
+      let temp = arr[i]
+      arr[i] = arr[minimum]
+      arr[minimum] = temp
+    }
+  }
+  return arr
+}
+
+console.log(selectionSort([5, 6, 2, 4]))
